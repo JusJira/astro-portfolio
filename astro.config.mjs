5eee1b3859df77ee9b5a60fe24e0bb721ed38bdd
JustinJira <jus.jira@gmail.com>
2024-01-26 08:29:15 +0700

    build: update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,20 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
+import node from "@astrojs/node";
 
 import cloudflare from "@astrojs/cloudflare";
 
+let adapter = cloudflare({});
+if (process.argv[3] === "--node" || process.argv[4] === "--node") {
+  adapter = node({ mode: "standalone" });
+}
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://imjustin.dev',
+  site: "https://imjustin.dev",
   integrations: [mdx(), sitemap(), tailwind(), preact()],
-  output: "hybrid",
-  adapter: cloudflare({
-    mode: 'directory',
-    routes: {
-      strategy: 'include',
-      include: ['/blog/*'], // handled by custom function: functions/users/[id].js
-    },
-})
+  output: "server",
+  adapter: adapter,
 });
