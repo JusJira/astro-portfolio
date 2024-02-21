c8f54b4c45d0965679402dff3630c3c182ef8ae0
JustinJira <jus.jira@gmail.com>
2024-02-21 21:14:15 +0700

    feat: integrate tailwind typography

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,5 +3,5 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {},
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
