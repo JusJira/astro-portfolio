5230041eee9d304ba4af0f3ae47550133d2826d2
JusJira <jus.jira@gmail.com>
2024-09-13 12:52:30 +0700

    style: updating some styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://imjustin.dev",
-  integrations: [mdx(), sitemap(), tailwind(), preact()],
+  integrations: [mdx(), sitemap(), tailwind(), preact(), icon()],
   redirects: {
     "/cu-future-classroom":
       "https://docs.google.com/forms/d/e/1FAIpQLSfcFRh0Qn5FMI7ScVR6PaI-hd91bRZ8BdfvitXUMoBOsR68WQ/viewform?usp=sf_link",
