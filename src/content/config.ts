29ce4dafedd1716f5123f50409eafa40e9d7c66e
JusJira <jus.jira@gmail.com>
2024-11-20 15:45:50 +0700

    feat: projects order

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,25 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const projects = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		shortDescription: z.string(),
-		featured: z.boolean(),
-		githubUrl: z.string().optional(),
-		websiteUrl: z.string().optional()
-	}),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    shortDescription: z.string(),
+    featured: z.boolean(),
+    githubUrl: z.string().optional(),
+    websiteUrl: z.string().optional(),
+    order: z.number(),
+  }),
 });
 
 const blog = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		datePublished: z.date(),
-		featured: z.boolean(),
-		heroImage: z.string(),
-	}),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    datePublished: z.date(),
+    featured: z.boolean(),
+    heroImage: z.string(),
+  }),
 });
 
 export const collections = { projects, blog };
