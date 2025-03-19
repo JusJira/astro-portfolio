fe16549c2aad394eb3bb97ac47efccb02edf26af
JusJira <jus.jira@gmail.com>
2025-03-19 20:47:57 +0700

    chore: remove unused

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
 import tailwind from "@astrojs/tailwind";
-import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: "https://imjustin.dev",
-  integrations: [mdx(), sitemap(), tailwind(), preact(), icon()],
+  integrations: [mdx(), sitemap(), tailwind(), preact()],
   redirects: {
     "/cu-future-classroom":
       "https://docs.google.com/forms/d/e/1FAIpQLSfcFRh0Qn5FMI7ScVR6PaI-hd91bRZ8BdfvitXUMoBOsR68WQ/viewform?usp=sf_link",
