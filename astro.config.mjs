a95700b02ff6dee337fa49ee586d4d236523a005
JusJira <jus.jira@gmail.com>
2025-11-26 00:24:26 +0700

    feat: upgrade to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
-import tailwind from "@astrojs/tailwind";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: "https://imjustin.dev",
-  integrations: [mdx(), sitemap(), tailwind(), preact()],
+  integrations: [mdx(), sitemap(), preact()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
