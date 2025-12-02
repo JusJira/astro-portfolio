d9c854e1c1bc5e79de823be185c263e88027109c
JustinJira <jus.jira@gmail.com>
2025-12-02 18:02:15 +0700

    Redesign (#6)
    * chore: upgrade
    
    * feat: website redesign
    
    * chore: clean up
    
    * style: spacing
    
    * feat: font optimization
    
    * style: delay notation after font load
    
    * feat: perf upgrade
    
    * feat: upgrade to tailwind v4
    
    * style: add some new
    
    * feat: light mode
    
    * chore: clean up

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,16 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import preact from "@astrojs/preact";
-import tailwind from "@astrojs/tailwind";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
   site: "https://imjustin.dev",
-  integrations: [mdx(), sitemap(), tailwind(), preact()],
-  redirects: {
-    "/cu-future-classroom":
-      "https://docs.google.com/forms/d/e/1FAIpQLSfcFRh0Qn5FMI7ScVR6PaI-hd91bRZ8BdfvitXUMoBOsR68WQ/viewform?usp=sf_link",
+  integrations: [mdx(), sitemap(), preact()],
+
+  vite: {
+    plugins: [tailwindcss()],
   },
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,14 @@
 import { defineCollection, z } from "astro:content";
 
+const iconNames = ["link", "bus", "qrCode", "disc3"] as const;
+
+// Derive a TypeScript type from the array
+export type IconName = (typeof iconNames)[number];
+
 const projects = defineCollection({
   schema: z.object({
     title: z.string(),
+    iconName: z.enum(iconNames).optional(),
     description: z.string(),
     shortDescription: z.string(),
     featured: z.boolean(),
